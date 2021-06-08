6b0e73658e0c82d6459a70db293078b668a18132
Alain Jouve <alain.jouve@wanadoo.fr>
2021-06-08 18:24:44 +0200

    Commit2 redemarrage de olymphys-4

---
+++ src/Form/Filter/LivredorFilterType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form\Filter;
+
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Form\Filter\Type\FilterType;
+use EasyCorp\Bundle\EasyAdminBundle\Form\Filter\Type\FilterTypeTrait;
+//use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType ; 
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Entity\Edition ;
+use App\Entity\Livredor;
+
+
+
+
+class LivredorFilterType extends FilterType
+{   public function __construct(SessionInterface $session)
+        {
+            $this->session = $session;
+            
+        }
+    
+    public function filter(QueryBuilder $queryBuilder, FormInterface $form, array $metadata)
+    { 
+
+        $datas =$form->getParent()->getData();
+
+      if(null!==$datas['edition']){
+            
+         $queryBuilder->andWhere( 'entity.edition =:edition')
+                      ->setParameter('edition',$datas['edition']);
+
+         $this->session->set('edition_titre',$datas['edition']->getEd()); 
+       }     
+
+       return $queryBuilder;
+         
+    }
+    
+     public function configureOptions(OptionsResolver $resolver)
+    {    $resolver->setDefaults([
+            'choice_label' => [
+                'Edition' => 'edition',
+
+
+            ],
+        ]);
+       
+    }
+
+    public function getParent()
+    {
+        return EntityType::class;
+    }
+
+   
+}
+
+
+

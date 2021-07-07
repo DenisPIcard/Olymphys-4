3ef16b2a11b0ce3653ae65819821fb599bc05716
Alain Jouve <alain.jouve@wanadoo.fr>
2021-07-07 20:13:11 +0200

    olymphys-4-easyadmin -professeurs

--- src/Controller/Admin/ProfesseursCrudController.php
+++ src/Controller/Admin/ProfesseursCrudController.php
@@ -23,8 +23,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Provider\AdminContextProvider;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\String\UnicodeString;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProfesseursCrudController extends AbstractCrudController
 {
@@ -35,7 +37,7 @@ class ProfesseursCrudController extends AbstractCrudController
     public function __construct(SessionInterface $session, AdminContextProvider $adminContextProvider)
     {
         $this->session = $session;
-        $this->adminContextProvider=$adminContextProvider;
+        $this->adminContextProvider = $adminContextProvider;
 
     }
 
@@ -54,8 +56,8 @@ class ProfesseursCrudController extends AbstractCrudController
             //->setPageTitle('index', 'Liste des équipe de la '.$editioned.$exp.' édition')
             ->setPageTitle(Crud::PAGE_DETAIL, 'Liste des professeurs')
             ->setSearchFields(['id', 'lettre', 'numero', 'titreProjet', 'nomLycee', 'denominationLycee', 'lyceeLocalite', 'lyceeAcademie', 'prenomProf1', 'nomProf1', 'prenomProf2', 'nomProf2', 'rne', 'contribfinance', 'origineprojet', 'recompense', 'partenaire', 'description'])
-            ->setPaginatorPageSize(50);
-            //->overrideTemplates(['layout' => 'bundles/EasyAdminBundle/list_profs.html.twig',]);
+            ->setPaginatorPageSize(50)
+            ->overrideTemplates(['layout' => 'bundles/EasyAdminBundle/list_profs.html.twig',]);
 
 
     }
@@ -93,49 +95,209 @@ class ProfesseursCrudController extends AbstractCrudController
             return [$prenom, $nom, $nomLycee, $lyceeLocalite, $lyceeAcademie, $rne, $equipes];
         }
     }
+
     public function configureFilters(Filters $filters): Filters
     {
         return $filters
             ->add(CustomEditionFilter::new('edition'));
 
     }
+
     public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
     {
         $context = $this->adminContextProvider->getContext();
-        $repositoryEdition=$this->getDoctrine()->getManager()->getRepository('App:Edition');
+        $repositoryEdition = $this->getDoctrine()->getManager()->getRepository('App:Edition');
 
         if ($context->getRequest()->query->get('filters') == null) {
-            $this->set_equipeString($this->session->get('edition'));
-            $qb = $this->get(EntityRepository::class)->createQueryBuilder($searchDto, $entityDto, $fields, $filters)
-                ->leftJoin('entity.equipes','eq')
-                ->andWhere('eq.edition =:edition')
-                ->setParameter('edition', $this->session->get('edition'));
+            $edition=$this->session->get('edition');
+
+        } else {
+            if (isset($context->getRequest()->query->get('filters')['edition'])) {
+
+                $idEdition = $context->getRequest()->query->get('filters')['edition'];
+                $edition = $repositoryEdition->findOneBy(['id' => $idEdition]);
+                $this->session->set('titreedition', $edition);
+            }
+
 
         }
-        else{
-            if (isset($context->getRequest()->query->get('filters')['edition'])){
-
-                $idEdition=$context->getRequest()->query->get('filters')['edition'];
-                $edition=$repositoryEdition->findOneBy(['id'=>$idEdition]);
-                $this->session->set('titreedition',$edition);}
-                $this->set_equipeString($edition);
-                $qb = $this->get(EntityRepository::class)->createQueryBuilder($searchDto, $entityDto, $fields, $filters)
-                    ->leftJoin('entity.equipes','eq')
-                    ->andWhere('eq.edition =:edition')
-                    ->setParameter('edition', $edition);
-        }
-
+        $qb = $this->get(EntityRepository::class)->createQueryBuilder($searchDto, $entityDto, $fields, $filters)
+            ->leftJoin('entity.equipes', 'eq')
+            ->andWhere('eq.edition =:edition')
+            ->setParameter('edition', $edition)
+            ->leftJoin('entity.user', 'u')
+            ->orderBy('u.nom', 'ASC');;
+        $this->set_equipeString($edition, $qb);
         return $qb;
     }
-    public function set_equipeString($edition){
-        $repositoryEquipesadmin=$this->getDoctrine()->getManager()->getRepository('App:Equipesadmin');
-        $listeEquipes= $repositoryEquipesadmin->findBy(['edition'=>$edition]);
-        foreach ($listeEquipes as $equipe){
-            $Idprof1=$equipe->
 
+    public function set_equipeString($edition, $qb)
+    {//Equipesstring est un champ à contenu variable destiné à l'affichage des équipes d'un prof pour une session
+        $em = $this->getDoctrine()->getManager();
+        $repositoryEquipes = $this->getDoctrine()->getRepository('App:Equipesadmin');
+        $listProfs = $qb->getQuery()->getResult();
+        if ($listProfs != null) {
+            foreach ($listProfs as $prof) {
+                $equipestring = '';
+                $equipes = $repositoryEquipes->createQueryBuilder('e')
+                    ->where('e.edition =:edition')
+                    ->setParameter('edition', $edition)
+                    ->andWhere('e.idProf1 =:user OR e.idProf2 =:user')
+                    ->setParameter('user', $prof->getUser())
+                    ->getQuery()->getResult();
 
+                if ($equipes != null) {
+                    foreach ($equipes as $equipe) {
+                        if ($equipe->getIdProf1() == $prof->getUser()) {
+                            $encad = '(prof1)';
+                        }
+                        if ($equipe->getIdProf2() == $prof->getUser()) {
+                            $encad = '(prof2)';
+                        }
+                        $equipestring = $equipestring . $equipe->getTitreProjet() . $encad;
+                        if (next($equipes) != null) {
+                            $equipestring = $equipestring . ' || ';
+                        }
+                    }
+                    $prof->setEquipesstring($equipestring);
+                    $em->persist($prof);
+                    $em->flush();
+                }
+            }
 
         }
+
+
+    }
+
+    /**
+     * @Route("/Professeurs/editer_tableau_excel,{choix}", name="profs_tableau_excel")
+     */
+
+    public function editer_tableau_excel($choix){
+
+
+        $em = $this->getDoctrine()->getManager();
+        $repositoryEdition = $this->getDoctrine()->getRepository('App:Edition');
+        $repositoryEquipes = $this->getDoctrine()->getRepository('App:Equipesadmin');
+        $edition=$repositoryEdition->findOneBy(['id'=>$choix]);
+        $repositoryProfs = $this->getDoctrine()->getManager()->getRepository('App:Professeurs');
+
+        $queryBuilder =  $repositoryProfs->createQueryBuilder('p')
+            ->groupBy('p.user')
+            ->leftJoin('p.equipes','eqs')
+            ->andWhere('eqs.edition =:edition')
+            ->setParameter('edition', $edition)
+            ->leftJoin('p.user','u')
+            ->orderBY('u.nom','ASC');
+        $listProfs= $queryBuilder->getQuery()->getResult();
+
+        if($listProfs!=null){
+            foreach($listProfs as $prof){
+                $equipestring ='';
+
+                $equipes=$repositoryEquipes->createQueryBuilder('e')
+                    ->where('e.edition =:edition')
+                    ->setParameter('edition',$edition)
+                    ->andWhere('e.idProf1 =:user OR e.idProf2 =:user')
+                    ->setParameter('user',$prof->getUser())
+                    ->getQuery()->getResult();
+
+                if ($equipes!=null){
+                    foreach($equipes as $equipe){
+                        if ($equipe->getIdProf1()==$prof->getUser()){
+                            $encad='(prof1)';
+                        }
+                        if ($equipe->getIdProf2()==$prof->getUser()){
+                            $encad='(prof2)';
+                        }
+                        $equipestring =  $equipestring.$equipe->getTitreProjet().$encad;
+                        if (next($equipes)!=null){
+                            $equipestring=$equipestring."\n";
+                        }
+                    }
+                    $equipestring=count($equipes).'-'.$equipestring;
+                    $prof->setEquipesstring($equipestring);
+                    $em->persist($prof);
+                    $em->flush();
+                }
+            }
+        }
+
+
+        $spreadsheet = new Spreadsheet();
+        $spreadsheet->getProperties()
+            ->setCreator("Olymphys")
+            ->setLastModifiedBy("Olymphys")
+            ->setTitle("OdPF".$edition->getEd()."ème édition - professeurs encadrants")
+            ->setSubject("PROFESSEURS")
+            ->setDescription("Office 2007 XLSX Document pour comité")
+            ->setKeywords("Office 2007 XLSX")
+            ->setCategory("Test result file");
+
+        $sheet = $spreadsheet->getActiveSheet();
+        foreach(['A','B','C','D','E','F','G','H','I','J','K','L']as $letter){
+            $sheet->getColumnDimension($letter)->setAutoSize(true);
+
+        }
+        $sheet->setCellValue('A1', 'Professeurs de la '.$edition->getEd().'e'.' édition');
+
+        $ligne=2;
+
+
+        $sheet->setCellValue('A'.$ligne, 'Nom')
+            ->setCellValue('B'.$ligne, 'Prénom')
+            ->setCellValue('C'.$ligne, 'Adresse')
+            ->setCellValue('D'.$ligne, 'Ville')
+            ->setCellValue('E'.$ligne, 'Code Postal')
+            ->setCellValue('F'.$ligne, 'Courriel')
+            ->setCellValue('G'.$ligne, 'téléphone')
+            ->setCellValue('H'.$ligne, 'Code UAI')
+            ->setCellValue('I'.$ligne, 'Lycée')
+            ->setCellValue('J'.$ligne, 'Commune lycée')
+            ->setCellValue('K'.$ligne, 'Académie')
+            ->setCellValue('L'.$ligne, 'Equipes');;
+
+        $ligne +=1;
+
+        foreach ($listProfs as $prof)
+        {
+
+
+            $sheet->setCellValue('A'.$ligne,$prof->getUser()->getNom() )
+                ->setCellValue('B'.$ligne, $prof->getUser()->getPrenom())
+                ->setCellValue('C'.$ligne, $prof->getUser()->getAdresse())
+                ->setCellValue('D'.$ligne, $prof->getUser()->getVille())
+                ->setCellValue('E'.$ligne, $prof->getUser()->getCode())
+                ->setCellValue('F'.$ligne, $prof->getUser()->getEmail())
+                ->getCell('G'.$ligne)->setValueExplicit($prof->getUser()->getPhone(), \PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
+            $sheet->setCellValue('H' . $ligne, $prof->getUser()->getRneId()->getRne())
+                ->setCellValue('I' . $ligne, $prof->getUser()->getRneId()->getNom())
+                ->setCellValue('J' . $ligne, $prof->getUser()->getRneId()->getCommune());
+            $sheet->setCellValue('K'.$ligne, $prof->getUser()->getRneId()->getAcademie());
+
+            $equipesstring=explode('-',$prof->getEquipesstring());
+            $sheet ->getRowDimension($ligne)->setRowHeight(12.5*intval($equipesstring[0]));
+            $sheet ->getCell('L'.$ligne)->setValueExplicit($equipesstring[1],\PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);//'abc \n cde'
+            $sheet->getStyle('A'.$ligne.':L'.$ligne)->getAlignment()->setWrapText(true);
+            $ligne +=1;
+        }
+
+
+
+
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="professeurs.xls"');
+        header('Cache-Control: max-age=0');
+
+        $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xls($spreadsheet);
+        ob_end_clean();
+        $writer->save('php://output');
+
+
+
+
+
 
     }
 
